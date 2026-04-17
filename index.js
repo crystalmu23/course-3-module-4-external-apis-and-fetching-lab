cca848b1808b6d76243f0483a6c7f2cd5d67fdf2
Crystal <crystalmucheru@gmail.com>
2026-04-17 16:06:11 +0300

    save changes

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-// index.js
-const weatherApi = "https://api.weather.gov/alerts/active?area="
+const weatherApi = "https://api.weather.gov/alerts/active?area=";
 
-// Your code here!
+async function fetchWeatherAlerts(stateAbbr) {
+  try {
+    errorEl.textContent = "";
+    errorEl.classList.add("hidden");
+    display.innerHTML = "";
+
+    const response = await fetch(weatherApi + stateAbbr);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch weather alerts.");
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    displayAlerts(data, stateAbbr);
+
+    input.value = "";
+
+  } catch (err) {
+    console.log(err.message);
+    showError(err.message);
+  }
+}
